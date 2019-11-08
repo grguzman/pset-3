870efd0002b94eaff6553159c6243ca113b384c5
grguzman <55497579+grguzman@users.noreply.github.com>
2019-11-07 23:53:39 -0500

    Add files via upload

---
+++ src/cards.js
@@ -0,0 +1,212 @@
+const readlineSync = require("readline-sync");
+
+const c = readlineSync.question("\nEnter a playing card: ");
+const nameofcard = c.toUpperCase();
+
+if (nameofcard == "AS") {
+  console.log("\nAce of Spades.\n");
+}
+
+else if (nameofcard == "AC") {
+  console.log("\nAce of Clubs.\n");
+}
+
+else if (nameofcard == "AH") {
+  console.log("\nAce of Hearts.\n");
+}
+
+else if (nameofcard == "KD") {
+  console.log("\nKing of Diamonds.\n");
+}
+
+else if (nameofcard == "KS") {
+  console.log("\nKing of Spades.\n");
+}
+
+else if (nameofcard == "KH") {
+  console.log("\nKing of Hearts.\n");
+}
+
+else if (nameofcard == "KC") {
+  console.log("\nKing of Clubs.\n");
+}
+
+else if (nameofcard == "QD") {
+  console.log("\nQueen of Diamonds.\n");
+}
+
+else if (nameofcard == "QS") {
+  console.log("\nQueen of Spades.\n");
+}
+
+else if (nameofcard == "QH") {
+  console.log("\nQueen of Hearts\n");
+}
+
+else if (nameofcard == "QC") {
+  console.log("\nQueen of Clubs.\n");
+}
+
+else if (nameofcard == "JS") {
+  console.log("\nJack of Spades.\n");
+}
+
+else if (nameofcard == "JD") {
+  console.log("\nJack of Diamonds.\n");
+}
+
+else if (nameofcard == "JH") {
+  console.log("\nJack of Hearts.\n");
+}
+
+else if (nameofcard == "JC") {
+  console.log("\nJack of Clubs.\n");
+}
+
+else if (nameofcard == "TS") {
+  console.log("\nTen of Spades.\n");
+}
+
+else if (nameofcard == "TD") {
+  console.log("\nTen of Diamonds.\n");
+}
+
+else if (nameofcard == "TH") {
+  console.log("\nTen of Hearts.\n");
+}
+
+else if (nameofcard == "TC") {
+  console.log("\nTen of Clubs.\n");
+}
+
+else if (nameofcard == "9S") {
+  console.log("\nNine of Spades.\n");
+}
+
+else if (nameofcard == "9D") {
+  console.log("\nNine of Diamonds.\n");
+}
+
+else if (nameofcard == "9H") {
+  console.log("\nNine of Hearts.\n");
+}
+
+else if (nameofcard == "9C") {
+  console.log("\nNine of Clubs.\n");
+}
+
+else if (nameofcard == "8S") {
+  console.log("\nEight of Spades.\n");
+}
+
+else if (nameofcard == "8D") {
+  console.log("\nEight of Diamonds.\n");
+}
+
+else if (nameofcard == "8H") {
+  console.log("\nEight of Hearts.\n");
+}
+
+else if (nameofcard == "8C") {
+  console.log("\nEight of Clubs.\n");
+}
+
+else if (nameofcard == "7S") {
+  console.log("\nSeven of Spades.\n");
+}
+
+else if (nameofcard == "7D") {
+  console.log("\nSeven of Diamonds.\n");
+}
+
+else if (nameofcard == "7H") {
+  console.log("\nSeven of Hearts.\n");
+}
+
+else if (nameofcard == "7C") {
+  console.log("\nSeven of Clubs.\n");
+}
+
+else if (nameofcard == "6S") {
+  console.log("\nSix of Spades.\n");
+}
+
+else if (nameofcard == "6D") {
+  console.log("\nSix of Diamonds.\n");
+}
+
+else if (nameofcard == "6H") {
+  console.log("\nSix of Hearts.\n");
+}
+
+else if (nameofcard == "6C") {
+  console.log("\nSix of Clubs.\n");
+}
+
+else if (nameofcard == "5S") {
+  console.log("\nFive of Spades.\n");
+}
+
+else if (nameofcard == "5D") {
+  console.log("\nFive of Diamonds.\n");
+}
+
+else if (nameofcard == "5H") {
+  console.log("\nFive of Hearts.\n");
+}
+
+else if (nameofcard == "5C") {
+  console.log("\nFive of Clubs.\n");
+}
+
+else if (nameofcard == "4S") {
+  console.log("\nFour of Spades.\n");
+}
+
+else if (nameofcard == "4D") {
+  console.log("\nFour of Diamonds.\n");
+}
+
+else if (nameofcard == "4H") {
+  console.log("\nFour of Hearts.\n");
+}
+
+else if (nameofcard == "4C") {
+  console.log("\nFour of Clubs.\n");
+}
+
+else if (nameofcard == "3S") {
+  console.log("\nThree of Spades.\n");
+}
+
+else if (nameofcard == "3D") {
+  console.log("\nThree of Diamonds.\n");
+}
+
+else if (nameofcard == "3H") {
+  console.log("\nThree of Hearts.\n");
+}
+
+else if (nameofcard == "3C") {
+  console.log("\nThree of Clubs.\n");
+}
+
+else if (nameofcard == "2S") {
+  console.log("\nTwo of Spades.\n");
+}
+
+else if (nameofcard == "2D") {
+  console.log("\nTwo of Diamonds.\n");
+}
+
+else if (nameofcard == "2H") {
+  console.log("\nTwo of Hearts.\n");
+}
+
+else if (nameofcard == "2C") {
+  console.log("\nTwo of Clubs.\n");
+}
+
+else {
+  console.log("\nInvalid\n");
+}
